6023b4efcfd12d67faa82c27fa272d0678242312
Marcelo Gonçalves <juninho_toh@hotmail.com>
2023-11-14 11:05:21 -0300

    feat: adição de novas funcionalidades

--- acerte-o-sapo-jogo/src/js/index.js
+++ acerte-o-sapo-jogo/src/js/index.js
@@ -3,6 +3,9 @@ O que fazer?
 1. Mudar temática do jogo (Feito!)
 2. Criar um sistema de vidas para o jogo (Feito!)
 3. Acelerar a velocidade do tempo de exibição da imagem (Feito!)
+4. Adicionar tela de game over (Feito!)
+5. Dividir a pontuação por rodadas (Feito!)
+6. Adicionar animações 
 */
 
 const state = {
@@ -13,8 +16,10 @@ const state = {
         score: document.querySelector("#score"),
         lifeCounter: document.querySelector("#life"),
         character: document.querySelector(".menu-lives h2 img"),
+        gameOverScreen: document.querySelector(".game-over"),
     },
     values: {
+        pontuacao: [],
         hitPosition: 0,
         result: 0,
         currentTime: 20,
@@ -36,7 +41,21 @@ function GameOver() {
     clearInterval(state.actions.countdownTimerId);
     clearInterval(state.actions.timerId);
     playSound("game-over");
-    alert("Game Over! O seu resultado foi: " + state.values.result + " pontos!");
+    const pontuacaoFinal = state.values.pontuacao.reduce((acumulador, value) => acumulador + value, 0);
+
+    const gameOver = `
+    <h2>GAME OVER!</h2>
+    <p>Resultado final...</p>
+    <ol>
+        <li>1ºRodada: ${state.values.pontuacao[0]}pts</li>
+        <li>2ºRodada: ${state.values.pontuacao[1]}pts</li>
+        <li>3ºRodada: ${state.values.pontuacao[2]}pts</li>
+    </ol>
+    <h3>Pontuação total: ${pontuacaoFinal}pts</h3>
+    `
+    state.view.gameOverScreen.innerHTML = gameOver; 
+    state.view.gameOverScreen.style.display = "block";
+    state.values.pontuacao = [];
 }
 
 function countdown() {
@@ -44,14 +63,20 @@ function countdown() {
     state.view.timeLeft.textContent = `${state.values.currentTime}s`;
 
     if(state.values.currentTime <= 0) {
-        if(state.values.life <= 0) {
-            GameOver();
-        } else {
+        if(state.values.life > 0) {
+            state.values.pontuacao.push(state.values.result);
+            state.values.result = 0;
             state.values.life--;
+            state.view.character.style.opacity = ".5"
             state.view.lifeCounter.textContent = `x${state.values.life}`
             playSound("loss-life");
             state.values.currentTime = 20;
         };
+        if(state.values.life < 1) {
+            GameOver();
+        };
+    } else {
+        state.view.character.style.opacity = "1";
     };
 };
 
